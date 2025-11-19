86a67e6f0e59235e22e3d5909dcefa5f5afcfc12
bautilemoine <blemoine@udesa.edu.ar>
2025-11-18 22:53:33 -0300

    login.js

---
+++ js/login.js
@@ -0,0 +1,25 @@
+let form=document.querySelector(".registro")
+let emailinput=document.querySelector("#email")
+let contrasenainput=document.querySelector("#contrasena")
+
+
+form.addEventListener("submit",function(event){
+event.preventDefault();
+
+
+
+if (emailinput.value=="" ){
+    alert ("El campo email es obligatorio");
+    return;
+}
+
+if (contrasenainput.value.length<6){
+    alert ("El campo contrasena es obligatorio");
+    return;
+};
+
+localStorage.setItem("email", emailinput.value); 
+
+location.href="./index.html";
+
+})
