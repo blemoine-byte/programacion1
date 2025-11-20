69a28a3c056727511e4f9e07d541e8c3d76f1a45
bautilemoine <blemoine@udesa.edu.ar>
2025-11-20 11:19:19 -0300

    register terminado

--- js/register.js
+++ js/register.js
@@ -1,6 +1,7 @@
 let form=document.querySelector(".registro")
 let emailinput=document.querySelector("#email")
 let contrasenainput=document.querySelector("#contrasena")
+let repetircontrasenainput=document.querySelector("#repetircontrasena")
 
 
 form.addEventListener("submit",function(event){
@@ -18,12 +19,15 @@ if (contrasenainput.value==""){
     return;
 }
 
-
 if (contrasenainput.value.length<6){
-    alert ("La longitud minima de la contrasena es 6 caracteres");
+    alert ("El campo contrasena es obligatorio");
     return;
 };
 
+if (contrasenainput!==repetircontrasenainput){
+    alert ("Las contrasenas no coinciden");
+    return;
+}
 localStorage.setItem("email", emailinput.value); 
 
 location.href="./index.html";
